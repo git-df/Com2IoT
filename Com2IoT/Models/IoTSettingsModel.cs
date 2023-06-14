f9928222469b1d34a6ae198a9f7d7cc8e33f3292
Dawid Florian <dawidflorian99@gmail.com>
2023-06-14 09:32:26 +0200

    add-models

---
+++ Com2IoT/Models/IoTSettingsModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com2IoT.Models
+{
+    public class IoTSettingsModel
+    {
+        public string DivId { get; set; }
+        public string DivKey { get; set; }
+        public string HostName { get; set; }
+
+        public IoTSettingsModel()
+        {
+            DivKey = "";
+            HostName = "";
+            DivId = "";
+        }
+
+        public IoTSettingsModel(string id, string key, string host)
+        {
+            DivKey = key;
+            HostName = host;
+            DivId = id;
+        }
+    }
+}
---
+++ Com2IoT/Models/MessageModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com2IoT.Models
+{
+    public class MessageModel
+    {
+        public DateTime Sended { get; set; }
+        public string Message { get; set; }
+
+        public MessageModel(string data)
+        {
+            Message = data;
+            Sended = DateTime.UtcNow;
+        }
+    }
+}
