58890c6158b9c094ed0d33e06e373a33bc90c7aa
Dawid Florian <dawidflorian99@gmail.com>
2023-06-14 09:34:34 +0200

    add-services

---
+++ Com2IoT/Services/FileService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com2IoT.Services
+{
+    public class FileService
+    {
+        public string fileUrl { get; set; } = string.Empty;
+        public FileService()
+        {
+            UpdateFileUrl();
+        }
+
+        public void UpdateFileUrl()
+        {
+            fileUrl = @".\logs\" + DateTime.Now.ToString("d") + ".txt";
+        }
+
+        public void WriteFIle(string data)
+        {
+            if (!Directory.Exists(@".\logs"))
+            {
+                Directory.CreateDirectory(@".\logs");
+            }
+
+            StreamWriter sw = new StreamWriter(fileUrl, true, Encoding.ASCII);
+            sw.WriteLine("[" + DateTime.UtcNow.ToString("G") + "] " + data);
+            sw.Close();
+        }
+    }
+}
---
+++ Com2IoT/Services/IoTHubService.cs
@@ -0,0 +1,92 @@
+﻿using Com2IoT.Models;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com2IoT.Services
+{
+    public class IoTHubService
+    {
+        public string DevId { get; set; } = string.Empty;
+        public string DevKey { get; set; } = string.Empty;
+        public string HostName { get; set; } = string.Empty;
+        public bool IsEnable { get; set; }
+
+        public IoTHubService()
+        {
+            IsEnable = false;
+        }
+
+        public async Task<bool> StopIoT()
+        {
+            if (IsEnable)
+            {
+                bool good = await SendToIoT("Stop komunikacji z IoT");
+                if (good)
+                {
+                    IsEnable = false;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> StartIoT(string dId, string dKey, string hostIoT)
+        {
+            DevId = dId;
+            DevKey = dKey;
+            HostName = hostIoT;
+
+            if (!IsEnable)
+            {
+                bool good = await SendToIoT("Start komunikacji z IoT");
+                if (good)
+                {
+                    IsEnable = true;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> SendToIoT(string data)
+        {
+            try
+            {
+                MessageModel messageModel = new MessageModel(data);
+                var authenticationKey = new DeviceAuthenticationWithRegistrySymmetricKey(DevId, DevKey);
+                var divClient = DeviceClient.Create(HostName, authenticationKey, Microsoft.Azure.Devices.Client.TransportType.Mqtt);
+                string messageString = JsonConvert.SerializeObject(messageModel);
+                Microsoft.Azure.Devices.Client.Message message = new Microsoft.Azure.Devices.Client.Message(Encoding.ASCII.GetBytes(messageString));
+                await divClient.SendEventAsync(message);
+                await divClient.CloseAsync();
+                divClient.Dispose();
+                return true;
+
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+    }
+}
